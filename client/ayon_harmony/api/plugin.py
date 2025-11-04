407ca497e641f15ccb14ebdbdb49bcfd30f4a72a
Félix David <felixg.david@gmail.com>
2025-11-04 12:30:36 +0100

    lin

--- client/ayon_harmony/api/plugin.py
+++ client/ayon_harmony/api/plugin.py
@@ -176,7 +176,7 @@ class HarmonyRenderCreator(HarmonyCreator):
     It creates new Composite type node from which it is rendered.
     """
     product_type = "render"
-    
+
     node_type = "COMPOSITE"
     # should node be auto connected to main Composite node for Harmony Advanced
     auto_connect = False
